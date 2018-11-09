ee110249ab52fdef56667d033ec52dad3213ed86
Jakub Martyčák <martycakjakub@gmail.com>
2018-11-09 12:06:03 +0100

    Add typography - title component

---
+++ src/components/typography/title.jsx
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import cx from 'classnames';
+
+import style from './title.less';
+
+export const Title = ({ children, alignment }) => {
+  return (
+    <div class={cx({[style.title]: true, [style[alignment]]: !!alignment})}>
+      {children}
+    </div>
+  );
+};
