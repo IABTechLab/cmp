ab0ea15d1c90add5b01de0ac996944c46f3663b2
Jakub Martyčák <martycakjakub@gmail.com>
2018-11-09 12:59:41 +0100

    Add basic layout components

---
+++ src/components/layout/col.jsx
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import cx from 'classnames';
+
+import style from './layout.less';
+
+export const Col = ({ children, className }) => {
+  return (
+    <div class={cx(style.column, className)}>
+      {children}
+    </div>
+  );
+};
---
+++ src/components/layout/row.jsx
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import cx from 'classnames';
+
+import style from './layout.less';
+
+export const Row = ({ children, className }) => {
+  return (
+    <div class={cx(style.row, className)}>
+      {children}
+    </div>
+  );
+};
