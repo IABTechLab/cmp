63db4115c382643cc4a9ccc93d7ade763cbf41bf
Jakub Martyčák <martycakjakub@gmail.com>
2018-11-22 17:35:16 +0100

    Add paragraph component

---
+++ src/components/typography/paragraph.jsx
@@ -0,0 +1,9 @@
+import { h } from 'preact';
+import style from './paragraph.less';
+
+export const Paragraph = ({ children, ...rest }) =>(
+  <p class={style.paragraph} {...rest}>
+    {children}
+  </p>
+);
+
