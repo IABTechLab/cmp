278e73074afd53803a13e20057555099103ca779
Jakub Martyčák <martycakjakub@gmail.com>
2018-11-22 17:47:15 +0100

    Add index files for most components

--- src/components/chevron/chevron.jsx
+++ src/components/chevron/chevron.jsx
@@ -3,12 +3,10 @@ import cx from 'classnames';
 
 import style from './chevron.less';
 
-export const Chevron = ({ direction, onClick }) => {
-  return (
-    <span
-      name="ctrl"
-      class={cx(style.icon, style[direction])}
-      onClick={onClick}
-    />
-  );
-};
+export const Chevron = ({ direction, onClick }) => (
+  <span
+    name="ctrl"
+    class={cx(style.icon, style[direction])}
+    onClick={onClick}
+  />
+);
--- src/components/popup/intro/footer.jsx
+++ src/components/popup/intro/footer.jsx
@@ -1,15 +1,14 @@
 import { h } from 'preact';
 
-import Label from '../../label/label';
-import { Row } from '../../layout/row';
-import { Chevron } from '../../chevron/chevron';
+import { Label } from '../../label';
+import { Row } from '../../layout';
+import { Chevron } from '../../chevron';
 import style from './intro.less';
 
-export const Footer = ({ onShowSummary }) => {
-  return (
-    <Row className={style.footer}>
-      <Chevron onClick={onShowSummary} />
-      <Label localizeKey="footer.message" class="primaryText" />
-    </Row>
-  );
-};
+export const Footer = ({ onShowSummary }) =>  (
+  <Row className={style.footer}>
+    <Chevron onClick={onShowSummary} />
+    <Label localizeKey="footer.message" class="primaryText" />
+  </Row>
+);
+
