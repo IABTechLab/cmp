99783d14ffa5490e5da0f4d80b0960e04e72b932
Jakub Martyčák <martycakjakub@gmail.com>
2018-11-23 09:20:35 +0100

    Improve typography components

--- src/components/typography/paragraph.jsx
+++ src/components/typography/paragraph.jsx
@@ -1,9 +1,8 @@
 import { h } from 'preact';
 import style from './paragraph.less';
 
-export const Paragraph = ({ children, ...rest }) =>(
+export const Paragraph = ({ children, ...rest }) => (
   <p class={style.paragraph} {...rest}>
     {children}
   </p>
 );
-
--- src/components/typography/title.jsx
+++ src/components/typography/title.jsx
@@ -3,9 +3,12 @@ import cx from 'classnames';
 
 import style from './title.less';
 
-export const Title = ({ children, alignment }) => {
+export const Title = ({ children, alignment, ...rest }) => {
   return (
-    <div class={cx({ [style.title]: true, [style[alignment]]: !!alignment })}>
+    <div
+      class={cx({ [style.title]: true, [style[alignment]]: !!alignment })}
+      {...rest}
+    >
       {children}
     </div>
   );
