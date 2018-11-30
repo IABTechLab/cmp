efc7200b8db3b84fd8c4fe9fd73db092b5a197fc
Jakub Martyčák <martycakjakub@gmail.com>
2018-11-30 19:26:57 +0100

    Add theming to ul component

--- src/components/ul/ul.jsx
+++ src/components/ul/ul.jsx
@@ -1,15 +1,20 @@
 import { h } from 'preact';
+import PropTypes from 'prop-types';
 
 import style from './ul.less';
 
 const itemFormatter = val => val.toString();
 
-export const Ul = ({ items = [], formatItem = itemFormatter }) => (
+export const Ul = ({ items = [], formatItem = itemFormatter }, { theme }) => (
   <ul class={style.ul}>
     {items.map((item, i) => (
-      <li class={style.li} key={i}>
+      <li class={style.li} key={i} style={{ color: theme.colorTextPrimary }}>
         {formatItem(item)}
       </li>
     ))}
   </ul>
 );
+
+Ul.contextTypes = {
+  theme: PropTypes.object.isRequired,
+};
