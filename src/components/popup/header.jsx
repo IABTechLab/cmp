f974350376c3e3c0370651619db7d28300b2862d
Jakub Martyčák <martycakjakub@gmail.com>
2018-11-30 19:29:10 +0100

    Temporarily change theme and add theming to header

--- src/components/popup/header.jsx
+++ src/components/popup/header.jsx
@@ -4,13 +4,17 @@ import { Label } from '../label';
 import { Chevron } from '../chevron';
 import { Title } from '../typography';
 import { Row } from '../layout';
+import { Divider } from '../divider';
 import style from './popup.less';
 
 export const Header = ({ titleKey, showChevron, onChevronClick }) => (
-  <Row className={style.header}>
-    {showChevron && <Chevron direction="down" onClick={onChevronClick} />}
-    <Title>
-      <Label localizeKey={titleKey} />
-    </Title>
-  </Row>
+  <div>
+    <Row className={style.header}>
+      {showChevron && <Chevron direction="down" onClick={onChevronClick} />}
+      <Title>
+        <Label localizeKey={titleKey} />
+      </Title>
+    </Row>
+    <Divider />
+  </div>
 );
