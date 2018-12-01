f39750ce5ac07de32e97946e52f8bea63e7729f3
Jakub Martyčák <martycakjakub@gmail.com>
2018-12-01 19:44:30 +0100

    Imrpove ul component

--- src/components/ul/ul.jsx
+++ src/components/ul/ul.jsx
@@ -5,16 +5,20 @@ import style from './ul.less';
 
 const itemFormatter = val => val.toString();
 
-export const Ul = ({ items = [], formatItem = itemFormatter }, { theme }) => (
+export const Li = ({ children, ...rest }, { theme }) => (
+  <li style={{ color: theme.colorTextPrimary }} class={style.li} {...rest}>
+    {children}
+  </li>
+);
+
+Li.contextTypes = {
+  theme: PropTypes.object.isRequired,
+};
+
+export const Ul = ({ items = [], formatItem = itemFormatter }) => (
   <ul class={style.ul}>
     {items.map((item, i) => (
-      <li class={style.li} key={i} style={{ color: theme.colorTextPrimary }}>
-        {formatItem(item)}
-      </li>
+      <Li key={i}>{formatItem(item)}</Li>
     ))}
   </ul>
 );
-
-Ul.contextTypes = {
-  theme: PropTypes.object.isRequired,
-};
