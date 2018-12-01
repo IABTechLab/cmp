b59bca6f319c3447d0375075b730825db2f743fa
Jakub Martyčák <martycakjakub@gmail.com>
2018-12-01 17:54:19 +0100

    Add theming to puprose list component

--- src/components/popup/details/purposes/list.jsx
+++ src/components/popup/details/purposes/list.jsx
@@ -1,32 +1,52 @@
 import { h } from 'preact';
 import cx from 'classnames';
+import PropTypes from 'prop-types';
 
 import { Label } from '../../../label';
 import style from './purposes.less';
 
-export const PurposeList = ({
-  allPurposes,
-  selectedPurposeIndex,
-  purposes,
-  onPurposeClick,
-}) => (
+export const PurposeList = (
+  { allPurposes, selectedPurposeIndex, purposes, onPurposeClick },
+  { theme },
+) => (
   <div class={style.purposeList}>
-    {allPurposes.map((purpose, i) => (
-      <div
-        class={cx({
-          [style.purposeItem]: true,
-          [style.selectedPurpose]: selectedPurposeIndex === i,
-        })}
-        onClick={onPurposeClick(i)}
-      >
-        <Label
-          localizeKey={`purposes.${
-            i >= purposes.length ? 'customPurpose' : 'purpose'
-          }${purpose.id}.menu`}
+    {allPurposes.map((purpose, i) => {
+      const isActive = selectedPurposeIndex === i;
+      const itemStyles = isActive
+        ? {
+            backgroundColor: theme.colorBackground,
+            color: theme.colorTextPrimary,
+            borderColor: theme.colorBorder,
+          }
+        : {
+            backgroundColor: theme.colorPrimary,
+            color: 'white',
+            borderColor: theme.colorBorder,
+            borderRightWidth: 1,
+            borderRightStyle: 'solid',
+          };
+      return (
+        <div
+          class={cx({
+            [style.purposeItem]: true,
+            [style.selectedPurpose]: isActive,
+          })}
+          style={itemStyles}
+          onClick={onPurposeClick(i)}
         >
-          {purpose.name}
-        </Label>
-      </div>
-    ))}
+          <Label
+            localizeKey={`purposes.${
+              i >= purposes.length ? 'customPurpose' : 'purpose'
+            }${purpose.id}.menu`}
+          >
+            {purpose.name}
+          </Label>
+        </div>
+      );
+    })}
   </div>
 );
+
+PurposeList.contextTypes = {
+  theme: PropTypes.object.isRequired,
+};
