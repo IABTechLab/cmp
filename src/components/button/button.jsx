f68749d02eb1d1652cb287ad618d3fa7a45d6e94
Jakub Martyčák <martycakjakub@gmail.com>
2018-12-01 18:14:16 +0100

    Fix theming for button and chevron

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -16,7 +16,7 @@ export const Button = (
       }
     : {
         borderColor: theme.colorPrimary,
-        color: theme.colorBackground,
+        color: 'white',
         backgroundColor: theme.colorPrimary,
       };
   return (
--- src/components/popup/summary/purposes.jsx
+++ src/components/popup/summary/purposes.jsx
@@ -3,6 +3,7 @@ import cx from 'classnames';
 
 import { Label } from '../../label';
 import { Ul } from '../../ul';
+import { Text } from '../../typography';
 import style from '../summary/summary.less';
 
 const formatPurpose = purpose => purpose.name;
@@ -11,11 +12,13 @@ export const Purposes = ({ allPurposes }) => {
   return (
     <div class={style.content}>
       <Label
+        is={Text}
         localizeKey="footer.deviceInformationHeader"
         class={cx(style.subtitle, 'primaryText')}
       />
-      <Label localizeKey="footer.deviceInformation" class="primaryText" />
+      <Label localizeKey="footer.deviceInformation" />
       <Label
+        is={Text}
         localizeKey="footer.purposesHeader"
         class={cx(style.subtitle, 'primaryText')}
       />
