a6445c63476b7164035939b2b80a8d2580c006dd
Jakub Martyčák <martycakjakub@gmail.com>
2019-01-25 21:09:23 +0100

    Fix initilalization so globalConsentLocation is used from remote config

--- src/lib/init.js
+++ src/lib/init.js
@@ -16,171 +16,184 @@ import log from './log';
 import config from './config';
 const metadata = require('../../metadata.json');
 
+const getConsentData = () => {
+  return Promise.all([
+    readVendorConsentCookie(),
+    readPublisherConsentCookie(),
+  ]).then(([vendorConsentData, publisherConsentData]) => {
+    return { vendorConsentData, publisherConsentData };
+  });
+};
+
+const loadConfig = configUrl => {
+  if (configUrl) {
+    log.debug('load remote config from', config.remoteConfigUrl);
+    return fetch(configUrl)
+      .then(response => response.json())
+      .catch(err => ({}));
+  }
+  return Promise.resolve({});
+};
+
 export function init(configUpdates) {
   config.update(configUpdates);
   log.debug('Using configuration:', config);
 
   // Fetch the current vendor consent before initializing
-  return readVendorConsentCookie().then(vendorConsentData => {
-    return readPublisherConsentCookie()
-      .then(publisherConsentData => {
-        // Initialize the store with all of our consent data
-        const store = new Store({
-          vendorConsentData,
-          publisherConsentData,
-          cmpId: metadata.cmpId,
-          cmpVersion: metadata.cmpVersion,
-          cookieVersion: 1,
-        });
+  return loadConfig(config.remoteConfigUrl).then(
+    ({ vendors, purposes, features, vendorListVersion, ...rest }) => {
+      config.update(rest);
 
-        const loadVendorsAndPurposes = () => {
-          const _fetchLocalizedPurposeList =
-            store.consentLanguage.toLowerCase() === 'en'
-              ? Promise.resolve
-              : fetchLocalizedPurposeList;
+      return getConsentData()
+        .then(({ publisherConsentData, vendorConsentData }) => {
+          // Initialize the store with all of our consent data
+          const store = new Store({
+            vendorConsentData,
+            publisherConsentData,
+            cmpId: metadata.cmpId,
+            cmpVersion: metadata.cmpVersion,
+            cookieVersion: 1,
+          });
 
-          // Request lists
-          return Promise.all([
-            fetchVendorList().then(resp => {
-              store.updateVendorList(resp);
+          const loadVendorsAndPurposes = () => {
+            const _fetchLocalizedPurposeList =
+              store.consentLanguage.toLowerCase() === 'en'
+                ? Promise.resolve
+                : fetchLocalizedPurposeList;
 
-              _fetchLocalizedPurposeList().then(localized => {
-                localized && store.updateLocalizedPurposeList(localized);
-              });
-            }),
-            fetchCustomPurposeList().then(store.updateCustomPurposeList),
-          ]);
-        };
+            // Request lists
+            return Promise.all([
+              fetchVendorList().then(resp => {
+                store.updateVendorList(resp);
 
-        const loadRemoteConfig = () => {
-          log.debug('load remote config from', config.remoteConfigUrl);
-          return fetch(config.remoteConfigUrl)
-            .then(response => response.json())
-            .then(
-              ({ vendors, purposes, features, vendorListVersion, ...rest }) => {
-                config.update(rest);
-
-                if (!vendors || vendors.length === 0) {
-                  return fetchVendorList().then(res => {
-                    store.updateVendorList(res);
-                    store.updateLocalizedPurposeList({ purposes, features });
-                  });
-                }
-
-                store.updateVendorList({
-                  vendors,
-                  purposes,
-                  version: vendorListVersion,
+                _fetchLocalizedPurposeList().then(localized => {
+                  localized && store.updateLocalizedPurposeList(localized);
                 });
+              }),
+              fetchCustomPurposeList().then(store.updateCustomPurposeList),
+            ]);
+          };
+
+          const updateVendorsAndPurposes = () => {
+            if (!vendors || vendors.length === 0) {
+              return fetchVendorList().then(res => {
+                store.updateVendorList(res);
                 store.updateLocalizedPurposeList({ purposes, features });
-              },
-            )
-            .catch(err => {
-              log.error('remote config not loaded', err);
-              return loadVendorsAndPurposes();
-            });
-        };
-
-        const loadCmpConfigurationData = config.remoteConfigUrl
-          ? loadRemoteConfig
-          : loadVendorsAndPurposes;
-
-        return loadCmpConfigurationData()
-          .then(() => {
-            // Pull queued command from __cmp stub
-            const { commandQueue = [], onConfigLoaded } =
-              window[CMP_GLOBAL_NAME] || {};
-
-            if (typeof onConfigLoaded === 'function') {
-              onConfigLoaded(config);
-            }
-
-            // Replace the __cmp with our implementation
-            const cmp = new Cmp(store, config);
-            store.updateCmpHandle(cmp);
-
-            // Expose `processCommand` as the CMP implementation
-            window[CMP_GLOBAL_NAME] = cmp.processCommand;
-
-            // Execute any previously queued command
-            cmp.commandQueue = commandQueue;
-
-            // set cookies on digitrust domain after consent submitted
-            const { addEventListener, getVendorConsents } = cmp.commands;
-            if (config.digitrust.redirects) {
-              addEventListener('consentStringUpdated', digitrustRedirect);
-            }
-
-            function digitrustRedirect() {
-              getVendorConsents([64], result => {
-                if (
-                  result &&
-                  result.vendorConsents &&
-                  result.vendorConsents[64]
-                ) {
-                  window.location.replace(
-                    `${config.digitrustRedirectUrl}${encodeURIComponent(
-                      window.location.href,
-                    )}`,
-                  );
-                }
               });
             }
+            store.updateVendorList({
+              vendors,
+              purposes,
+              version: vendorListVersion,
+            });
+            store.updateLocalizedPurposeList({ purposes, features });
+            return Promise.resolve();
+          };
 
-            return checkIfUserInEU(config.geoIPVendor, response => {
-              cmp.gdprApplies = response.applies;
-              cmp.gdprAppliesLanguage = response.language;
-              cmp.gdprAppliesLocation = response.location;
-            })
-              .then(response => {
-                function addLocatorFrame() {
-                  if (!window.frames['__cmpLocator']) {
-                    if (document.body) {
-                      var frame = document.createElement('iframe');
-                      frame.style.display = 'none';
-                      frame.name = '__cmpLocator';
-                      document.body.appendChild(frame);
-                    } else {
-                      setTimeout(addLocatorFrame, 5);
+          const loadCmpConfigurationData = config.remoteConfigUrl
+            ? updateVendorsAndPurposes
+            : loadVendorsAndPurposes;
+
+          return loadCmpConfigurationData()
+            .then(() => {
+              // Pull queued command from __cmp stub
+              const { commandQueue = [], onConfigLoaded } =
+                window[CMP_GLOBAL_NAME] || {};
+
+              if (typeof onConfigLoaded === 'function') {
+                onConfigLoaded(config);
+              }
+
+              // Replace the __cmp with our implementation
+              const cmp = new Cmp(store, config);
+              store.updateCmpHandle(cmp);
+
+              // Expose `processCommand` as the CMP implementation
+              window[CMP_GLOBAL_NAME] = cmp.processCommand;
+
+              // Execute any previously queued command
+              cmp.commandQueue = commandQueue;
+
+              // set cookies on digitrust domain after consent submitted
+              const { addEventListener, getVendorConsents } = cmp.commands;
+              if (config.digitrust.redirects) {
+                addEventListener('consentStringUpdated', digitrustRedirect);
+              }
+
+              function digitrustRedirect() {
+                getVendorConsents([64], result => {
+                  if (
+                    result &&
+                    result.vendorConsents &&
+                    result.vendorConsents[64]
+                  ) {
+                    window.location.replace(
+                      `${config.digitrustRedirectUrl}${encodeURIComponent(
+                        window.location.href,
+                      )}`,
+                    );
+                  }
+                });
+              }
+
+              return checkIfUserInEU(config.geoIPVendor, response => {
+                cmp.gdprApplies = response.applies;
+                cmp.gdprAppliesLanguage = response.language;
+                cmp.gdprAppliesLocation = response.location;
+              })
+                .then(response => {
+                  function addLocatorFrame() {
+                    if (!window.frames['__cmpLocator']) {
+                      if (document.body) {
+                        var frame = document.createElement('iframe');
+                        frame.style.display = 'none';
+                        frame.name = '__cmpLocator';
+                        document.body.appendChild(frame);
+                      } else {
+                        setTimeout(addLocatorFrame, 5);
+                      }
                     }
                   }
-                }
 
-                addLocatorFrame();
-                store.updateIsEU(response.applies);
+                  addLocatorFrame();
+                  store.updateIsEU(response.applies);
 
-                // Render the UI
-                const App = require('../components/app').default;
-                render(
-                  <App
-                    store={store}
-                    cmp={cmp}
-                    notify={cmp.notify}
-                    config={config}
-                  />,
-                  document.body,
-                );
+                  // Render the UI
+                  const App = require('../components/app').default;
+                  render(
+                    <App
+                      store={store}
+                      cmp={cmp}
+                      notify={cmp.notify}
+                      config={config}
+                    />,
+                    document.body,
+                  );
 
-                // Notify listeners that the CMP is loaded
-                log.debug(
-                  `Successfully loaded CMP version: ${metadata.cmpVersion}`,
-                );
-                cmp.isLoaded = true;
-                cmp.notify('isLoaded');
-                cmp.cmpReady = true;
-                cmp.notify('cmpReady');
-                cmp.processCommandQueue();
-              })
-              .catch(err => {
-                log.error('Failed to check user location. CMP not ready', err);
-              });
-          })
-          .catch(err => {
-            log.error('Failed to load lists. CMP not ready', err);
-          });
-      })
-      .catch(err => {
-        log.error('Failed to load CMP', err);
-      });
-  });
+                  // Notify listeners that the CMP is loaded
+                  log.debug(
+                    `Successfully loaded CMP version: ${metadata.cmpVersion}`,
+                  );
+                  cmp.isLoaded = true;
+                  cmp.notify('isLoaded');
+                  cmp.cmpReady = true;
+                  cmp.notify('cmpReady');
+                  cmp.processCommandQueue();
+                })
+                .catch(err => {
+                  log.error(
+                    'Failed to check user location. CMP not ready',
+                    err,
+                  );
+                });
+            })
+            .catch(err => {
+              log.error('Failed to load lists. CMP not ready', err);
+            });
+        })
+        .catch(err => {
+          log.error('Failed to load CMP', err);
+        });
+    },
+  );
 }
