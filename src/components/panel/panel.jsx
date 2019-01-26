d80aee06940c585f414ac934beffb1fa0d62e4e5
Jakub Martyčák <martycakjakub@gmail.com>
2019-01-26 20:06:21 +0100

    Remove unnecessary div from Panel

--- src/components/panel/panel.jsx
+++ src/components/panel/panel.jsx
@@ -1,5 +1,4 @@
 import { h } from 'preact';
 
-export const Panel = ({ children = [], selectedIndex = 0, className }) => (
-  <div class={className}>{children[selectedIndex]}</div>
-);
+export const Panel = ({ children = [], selectedIndex = 0, className }) =>
+  children[selectedIndex];
