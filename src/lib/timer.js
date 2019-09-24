7129c70536a62af35846f23bbce3f40db79e5006
Petr Hoza <petr@cpex.cz>
2019-09-24 09:00:27 +0200

    change timer implementation

--- src/lib/timer.js
+++ src/lib/timer.js
@@ -2,9 +2,9 @@ import log from "./log";
 
 export const notifyTimer = event => {
 	try {
-		const cpexTimer = window.cpexTimer || {};
+		window.cpexTimer = cpexTimer || {};
 		cpexTimer.que = cpexTimer.que || [];
-		cpexTimer.que.push([event, performance.now()]);
+		cpexTimer.que.push(["event", performance.now()]);
 	} catch (e) {
 		log.error("Timer error: ", e);
 	}
