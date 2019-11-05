00cbb892516d9cae4b5194bec68a9fe6fb572f57
Petr Hoza <petr@cpex.cz>
2019-11-05 15:17:56 +0100

    timer update

--- src/lib/timer.js
+++ src/lib/timer.js
@@ -2,9 +2,8 @@ import log from "./log";
 
 export const notifyTimer = event => {
 	try {
-		window.cpexTimer = cpexTimer || {};
-		cpexTimer.que = cpexTimer.que || [];
-		cpexTimer.que.push(["event", performance.now()]);
+		console.log("Event:", event + " " + performance.now());
+		window.cpexTimer.que.push([event, performance.now()]);
 	} catch (e) {
 		log.error("Timer error: ", e);
 	}
